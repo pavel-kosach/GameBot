13659374dab2cb1136d6224ab81e15e2f65813fd
Pavel Kosach <kosachdev@gmail.com>
2023-02-05 14:10:01 +0300

    add difficult task

--- main.js
+++ main.js
@@ -1,30 +1,55 @@
 "use strict";
 
+let attempts = 10;
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const isNumber = function (num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
 function game() {
-  let aiNumber = 10;
-  let userNumber = prompt("Угадай число от 1 до 100");
+  let aiNumber = getRandomIntInclusive(1, 100);
+  console.log(aiNumber);
+  gameProcess();
 
-  if (userNumber == undefined) {
-    alert("игра окончена");
-  } else if (!isNumber(userNumber)) {
-    alert("Введите число!!!");
-    console.log(userNumber);
-    game();
-  } else if (userNumber > aiNumber && userNumber != null) {
-    alert("Загаданное число меньше");
-    console.log(userNumber);
-    game();
-  } else if (userNumber < aiNumber) {
-    alert("Загаданное число больше");
-    console.log(userNumber);
-    game();
-  } else if (userNumber == aiNumber) {
-    alert("Поздравляю, Вы угадали!!!");
-    console.log(userNumber);
+  function gameProcess() {
+    let userNumber = prompt("Угадай число от 1 до 100");
+
+    if (userNumber == undefined) {
+      alert("Игра окончена");
+    } else if (!isNumber(userNumber)) {
+      alert("Введи число!!!");
+      gameProcess();
+    } else if (userNumber > aiNumber && userNumber != null && attempts > 0) {
+      attempts = attempts - 1;
+      alert("Загаданное число меньше, осталось попыток " + attempts);
+      gameProcess();
+    } else if (userNumber < aiNumber && attempts > 0) {
+      attempts = attempts - 1;
+      alert("Загаданное число больше, осталось попыток " + attempts);
+      gameProcess();
+    } else if (userNumber == aiNumber && attempts > 0) {
+      let startGame = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+      if (startGame) {
+        attempts = 10;
+        game();
+      } else {
+        alert("Игра окончена");
+      }
+    } else if (attempts == 0) {
+      let startGame = confirm("Попытки закончились! Хотели бы сыграть еще?");
+      if (startGame) {
+        attempts = 10;
+        game();
+      } else {
+        alert("Игра окончена");
+      }
+    }
   }
 }
 
